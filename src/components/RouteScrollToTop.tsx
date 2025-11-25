701dd0c83d461339ef2f641d9c4979faec278167
kauann <marketingkauann@gmail.com>
2025-11-25 03:58:14 -0300

    fix: garantir scroll ao topo em navegaÃ§Ãµes

---
+++ src/components/RouteScrollToTop.tsx
@@ -0,0 +1,23 @@
+import { useLayoutEffect } from "react";
+import { useLocation } from "react-router-dom";
+
+const RouteScrollToTop = () => {
+  const { pathname, search } = useLocation();
+
+  useLayoutEffect(() => {
+    if (typeof window === "undefined") {
+      return;
+    }
+
+    window.scrollTo({
+      top: 0,
+      left: 0,
+      behavior: "auto",
+    });
+  }, [pathname, search]);
+
+  return null;
+};
+
+export default RouteScrollToTop;
+
